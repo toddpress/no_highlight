07d666006329a2db1149642271b1d7c39d7bec1c
Todd Pressley <todd.pressley1@gmail.com>
2020-11-09 16:37:46 -0500

    Create no_highlight.js

---
+++ no_highlight.js
@@ -0,0 +1,1 @@
+javascript:var sheet = (function() {var style = document.createElement('style'); style.appendChild(document.createTextNode('')); document.head.appendChild(style); return style.sheet;})();sheet.insertRule('*::selection { background-color: transparent !important; }', 0);
